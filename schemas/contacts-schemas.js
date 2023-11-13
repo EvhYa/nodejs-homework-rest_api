07ec4a9b2ad532c377d30050ab310730ffd94f9c
EvhYa <nicetea.bro@gmail.com>
2023-11-13 20:40:16 +0200

    fixes

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,9 +1,15 @@
 import Joi from "joi";
 
 export const contactSchema = Joi.object({
-   name: Joi.string().required(),
-   email: Joi.string().email().required(),
-   phone: Joi.string().required(),
+   name: Joi.string().required().messages({
+      "any.required": `missing required "name" field`,
+   }),
+   email: Joi.string().email().required().messages({
+      "any.required": `missing required "email" field`,
+   }),
+   phone: Joi.string().required().messages({
+      "any.required": `missing required "phone" field`,
+   }),
 });
 
 export const updContactSchema = Joi.object({
